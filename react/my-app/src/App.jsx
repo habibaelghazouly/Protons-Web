7726d8f6a3e019a5f9cf04c25c6bc51472451232
habibaelghazouly <habibahoussam7@gmail.com>
2025-08-26 01:55:25 +0300

    my first project

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import duck from './assets/download.jpeg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  function handleClick() {
+    alert("Button clicked!");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      
+      <div className="App">
+        <h1 className="title">Hello, React!</h1>
+        <button id="button" onClick={handleClick}>
+          Click Me
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+      <div className="pic">
+        <img src={duck} className="logo react" alt="React logo" />
+      </div>
+      
+      <div className="footer">
+        <p>© 2024 My React App</p>
+      </div>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
