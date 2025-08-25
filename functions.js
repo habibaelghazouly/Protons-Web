4faa423873b5e95cd657ce035b7ebcc64c0298f9
habibaelghazouly <habibahoussam7@gmail.com>
2025-08-26 01:00:59 +0300

    remove divide

--- functions.js
+++ functions.js
@@ -24,6 +24,5 @@ const addRandomNumbers = () => 3 + 4;
 console.log(add(2, 3)); // 5
 console.log(subtract(5, 2)); // 3
 console.log(multiply(3, 4)); // 12
-console.log(divide(10, 2)); // 5
 console.log(square(5)); // 25
 console.log(addRandomNumbers()); // 7
